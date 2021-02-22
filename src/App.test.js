82ca594f021587d941e851f1a0a32fe719a6831f
Dominic Chu <dominic.m.chu@gmail.com>
2021-02-22 11:55:03 -0500

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
@@ -6,8 +6,11 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
-test("renders without error", () => {}); 
-
+test("renders without error", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']"); 
+  expect(appComponent.length).toBe(1); 
+}); 
 test("renders button", () => {});
 test("renders counter display", () => {}); 
 test("counter starts at 0", () => {});
