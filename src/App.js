ed5288e19eb79f8752f03c1a486a3d6255417469
Dominic Chu <dominic.m.chu@gmail.com>
2021-02-22 12:20:58 -0500

    helper functions to do the repetitive and also for later in case of more args ie state

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 
 function App() {
   return (
-      <div data-test='component-app'>
-
-      </div>
+    <div data-test='component-app'>
+      <h1 data-test='counter-display'>The counter is currently</h1>
+      <button data-test='increment-button'>Increment Counter</button>
+    </div>
     );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -6,12 +6,32 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
+/**
+ * Factory function to create ShallowWrapper for App component.
+ * @fucntion setup 
+ * @returns {ShallowWrapper}
+*/
+const setup = () => shallow(<App />)
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
+
 test("renders without error", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']"); 
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent.length).toBe(1); 
 }); 
-test("renders button", () => {});
-test("renders counter display", () => {}); 
+
+test("renders button", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button'); 
+  expect(button.length).toBe(1); 
+});
+
+test("renders counter display", () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display'); 
+  expect(counterDisplay.length).toBe(1); 
+}); 
+
 test("counter starts at 0", () => {});
 test("clicking on the button increments counter display", () => {}); 
